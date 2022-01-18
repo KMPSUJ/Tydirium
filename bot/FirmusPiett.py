7a9e1261b3a8b7264c4d3e566abb081d4c6a117e
falcotinnunculus <61662934+falcotinnunculus@users.noreply.github.com>
2022-01-18 11:12:49 +0100

    Greeting method update, GENDER (#2)
    The bot checks the authors nick, and picks the greeting to use in the message.
    This method works only in case of polish names, and assumes that the first word of a nick is the first name.

--- bot/FirmusPiett.py
+++ bot/FirmusPiett.py
@@ -69,6 +69,15 @@ def startControllPanel(panel):
         print("Server stopped.")
 
 
+def gender(author):
+    first_name = author.display_name.split()[0]
+    name_ending = list(first_name)[-1]
+    if name_ending == 'a':
+        return "My Lady"
+    else:
+        return "Sir"
+
+
 class FirmusPiett(discord.Client):
     class Communicate:
         def __init__(self):
@@ -131,69 +140,71 @@ class FirmusPiett(discord.Client):
         if message.author == self.user:
             return
         if message.content.startswith(CMD_LEADER):
-            await self.execute_order(message.content[len(CMD_LEADER):], message.channel)
+            await self.execute_order(message.content[len(CMD_LEADER):], message.channel, message.author)
 
-    async def execute_order(self, cmd, channel):
+    async def execute_order(self, cmd, channel, author):
         HELP = "help"
         NEW_CODE = "new code"
         REPORT = "report"
         cmd = cmd.strip()
         if cmd.startswith(HELP):
-            await self.print_help(cmd[len(HELP):], channel)
+            await self.print_help(cmd[len(HELP):], channel, author)
         elif cmd.startswith(NEW_CODE):
-            await self.new_code(cmd[len(NEW_CODE):], channel)
+            await self.new_code(cmd[len(NEW_CODE):], channel, author)
         elif cmd.startswith(REPORT):
-            await self.report(cmd[len(REPORT):], channel)
+            await self.report(cmd[len(REPORT):], channel, author)
         else:
-            await self.bad_command(channel)
+            await self.bad_command(channel, author)
 
-    async def print_help(self, cmd, channel):
+    async def print_help(self, cmd, channel, author):
+        salutation = gender(author)
         cmd = cmd.strip()
         if cmd != "" and cmd != "!" and cmd != ".":
-            await self.bad_command(channel)
-        ans = "Calm down, Sir, I'm here.\n" \
+            await self.bad_command(channel, author)
+        ans = f"Calm down, {salutation}, I'm here.\n" \
               "You can ask me for `report` to see current state of the door " \
               "and last time when it was updated.\n" \
               "You can also order me to `new code n`, so I would display " \
               "the state of the door in different ways."
         await channel.send(ans)
 
-    async def new_code(self, cmd, channel):
+    async def new_code(self, cmd, channel, author):
+        salutation = gender(author)
         cmd = cmd.strip()
         try:
             cmd = int(cmd)
             if 0 <= cmd < len(self._communicate._communicates):
                 self._communicate.setCurrent(cmd)
                 self._last_code = -1
-                await channel.send("Yes Sir! Code {}".format(cmd))
+                await channel.send(f"Yes {salutation}! Code {cmd}")
             else:
-                await channel.send("Sir, code {} is out of the protocol!".format(cmd))
+                await channel.send(f"{salutation}, code {cmd} is out of the protocol!")
         except:
-            await self.bad_command(channel)
+            await self.bad_command(channel, author)
         
-    async def report(self, cmd, channel):
+    async def report(self, cmd, channel, author):
+        salutation = gender(author)
         cmd = cmd.strip()
         if cmd != "" and cmd != "!" and cmd != ".":
-            await self.bad_command(channel)
+            await self.bad_command(channel, author)
         await self.refreshStatus()
         ans = ""
         if self._last_code == 0:
-            ans += "Sir, according to our intelligence the door is closed.\n"
+            ans += f"{salutation}, according to our intelligence the door is closed.\n"
         elif self._last_code == 1:
-            ans += "Sir, according to our intelligence the door is open.\n"
+            ans += f"{salutation}, according to our intelligence the door is open.\n"
         else:
-            ans += "Sir, despite many Bothans died, we have not determined the state of the door.\n"
+            ans += f"{salutation}, despite many Bothans died, we have not determined the state of the door.\n"
         if ControllPanel.last_update is None:
             ans += "We haven't received any information from them yet."
         else:
             ans += "Last update from Tydirium was received "
             ans += ControllPanel.last_update.strftime("%d.%m.%Y at %H:%M:%S.")
         await channel.send(ans)
-        
-    async def bad_command(self, channel):
-        await channel.send("We have some communication disruption, Sir. Please repeat.")
 
-
+    async def bad_command(self, channel, author):
+        salutation = gender(author)
+        await channel.send(f"We have some communication disruption, {salutation}. Please repeat.")
 
 
 if __name__ == "__main__":
