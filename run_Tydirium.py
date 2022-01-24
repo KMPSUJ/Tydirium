df1616bd428203652c0fe3c8c1d87518ecf5341a
Bartosz Zbik <bartosz@zbik.org>
2022-01-24 21:53:53 +0100

    runs the server and piett

---
+++ run_Tydirium.py
@@ -0,0 +1,20 @@
+
+from http.server import HTTPServer
+from threading import Thread
+from bot.FirmusPiett import FirmusPiett
+from bot.TydiriumServer import startControllPanel, ControllPanel
+
+HOST_NAME = ""
+PORT = 7216
+PIETT_TOKEN = ""
+
+
+if __name__ == '__main__':
+    server = HTTPServer((HOST_NAME, PORT), ControllPanel)
+    server_thread = Thread(target=startControllPanel, args={server})
+    server_thread.start()
+
+    piett = FirmusPiett()
+    piett.run(PIETT_TOKEN)
+
+    server_thread.join()
