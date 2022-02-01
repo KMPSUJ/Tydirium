551b48453cda516c382a28bf4509ed5a9b07060f
Dawid Tracz <dawid.b.tracz@gmail.com>
2022-02-01 23:04:27 +0100

    reneme files to snake_case

--- run_tydirium.py
+++ run_tydirium.py
@@ -1,8 +1,8 @@
 
 from http.server import HTTPServer
 from threading import Thread
-from bot.FirmusPiett import FirmusPiett
-from bot.TydiriumServer import startControllPanel, ControllPanel
+from bot.firmus_piett import FirmusPiett
+from bot.tydirium_server import start_controll_panel, ControllPanel
 
 HOST_NAME = ""
 PORT = 7216
@@ -11,7 +11,7 @@ PIETT_TOKEN = ""
 
 if __name__ == '__main__':
     server = HTTPServer((HOST_NAME, PORT), ControllPanel)
-    server_thread = Thread(target=startControllPanel, args={server})
+    server_thread = Thread(target=start_controll_panel, args={server})
     server_thread.start()
 
     piett = FirmusPiett(HOST_NAME, PORT)
