08f202aaa599f388cec1e476d53f247a392817e5
Bartosz Zbik <bartosz@zbik.org>
2022-02-19 20:08:44 +0100

    add request timeout

--- bot/server_client.py
+++ bot/server_client.py
@@ -4,16 +4,16 @@ import requests
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
-def get_door_state(host, port):
-    request = requests.get(f"http://{host}:{port}/door-state")
+def get_door_state(host, port, timeout=10.0):
+    request = requests.get(f"http://{host}:{port}/door-state", timeout=timeout)
     content = request.text.split('\n')
     door_state = int(content[0])
     last_update = datetime.strptime(content[1], DATE_FORMAT)
     return door_state, last_update
 
 
-def get_door_state_prediction(host, port):
-    request = requests.get(f"http://{host}:{port}/door-state-prediction")
+def get_door_state_prediction(host, port, timeout=10.0):
+    request = requests.get(f"http://{host}:{port}/door-state-prediction", timeout=timeout)
     content = request.text.split('\n')
     door_state = int(content[0])
     return door_state
